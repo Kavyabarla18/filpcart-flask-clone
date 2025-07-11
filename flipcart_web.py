06c773506fe52b840af1836d9a039cc13ccfc36f
Kavyabarla18 <kavyabarla2007@gmail.com>
2025-07-11 15:28:06 +0530

    Update flipcart_web.py

--- flipcart_web.py
+++ flipcart_web.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import os 
 
-app = Flask(__name__, template_folder='html_pages')
+app = Flask(__name__, template_folder='.')
 
 @app.route('/')
 def login():
